024385aa42dd96dde4ba6daff90023fd91c960f3
joelberzalgithub <120808626+joelberzalgithub@users.noreply.github.com>
2024-02-17 21:20:45 +0100

    Segon commit de la meva app per capturar imatges amb app externa

--- app/src/main/java/com/example/pr22/MainActivity.java
+++ app/src/main/java/com/example/pr22/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.pr22;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.provider.MediaStore;
 import android.widget.Button;
@@ -10,9 +13,13 @@ import androidx.activity.result.ActivityResultLauncher;
 import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
 
+import java.util.Objects;
+
 public class MainActivity extends AppCompatActivity {
     private ImageView imageView;
     private ActivityResultLauncher<Intent> pickPictureLauncher;
+    private ActivityResultLauncher<String> requestCameraPermissionLauncher;
+    private ActivityResultLauncher<Intent> dispatchTakePictureIntentLauncher;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,7 +36,31 @@ public class MainActivity extends AppCompatActivity {
                     }
                 });
 
+        requestCameraPermissionLauncher = registerForActivityResult(new ActivityResultContracts.RequestPermission(),
+                isGranted -> {
+                    if (isGranted) {
+                        dispatchTakePictureIntentLauncher.launch(new Intent(MediaStore.ACTION_IMAGE_CAPTURE));
+                    }
+                });
+
+        dispatchTakePictureIntentLauncher = registerForActivityResult(new ActivityResultContracts.StartActivityForResult(),
+                result -> {
+                    if (result != null && result.getResultCode() == RESULT_OK) {
+                        assert result.getData() != null;
+                        imageView.setImageBitmap((Bitmap) Objects.requireNonNull(result.getData().getExtras()).get("data"));
+                    }
+                });
+
         Button galleryButton = findViewById(R.id.galleryButton);
         galleryButton.setOnClickListener(v -> pickPictureLauncher.launch(new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI)));
+
+        Button cameraButton = findViewById(R.id.cameraButton);
+        cameraButton.setOnClickListener(v -> {
+            if (checkSelfPermission(Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED) {
+                dispatchTakePictureIntentLauncher.launch(new Intent(MediaStore.ACTION_IMAGE_CAPTURE));
+            } else {
+                requestCameraPermissionLauncher.launch(Manifest.permission.CAMERA);
+            }
+        });
     }
 }
